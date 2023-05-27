92baa09764a5201c9707a040b1602e18bcdf1ed6
niclaspirkl <nimaxpirkl@gmail.com>
2023-05-27 19:52:39 +0200

    Added URL and Port Input

--- app/src/app/components/screen/screen.component.ts
+++ app/src/app/components/screen/screen.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'screen-view',
@@ -6,4 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./screen.component.css']
 })
 export class ScreenComponent {
+
+  iframeSrc: SafeResourceUrl;
+
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.iframeSrc = this.sanitizer.bypassSecurityTrustResourceUrl('');
+  }
+
+  update(value: string) {
+
+    if (value == "4200") {
+      this.iframeSrc = this.sanitizer.bypassSecurityTrustResourceUrl("http://localhost:4200/");
+
+    }
+    if (value == "5500") {
+      this.iframeSrc = this.sanitizer.bypassSecurityTrustResourceUrl("http://localhost:5500/");
+
+    } else {
+      this.iframeSrc = this.sanitizer.bypassSecurityTrustResourceUrl(value);
+
+    }
+
+  }
 }
