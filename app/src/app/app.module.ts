7ee49e3c74227ad48bf05723d9bf4684921c028d
niclaspirkl <nimaxpirkl@gmail.com>
2023-05-27 19:12:12 +0200

    Added Basic iframe

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ScreenComponent } from './components/screen/screen.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ScreenComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app/src/app/components/screen/screen.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'screen-view',
+  templateUrl: './screen.component.html',
+  styleUrls: ['./screen.component.css']
+})
+export class ScreenComponent {
+}
