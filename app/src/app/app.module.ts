15d03b7fed7a862f061cff999d7bcb72ec0694f3
niclaspirkl <nimaxpirkl@gmail.com>
2023-06-03 22:18:30 +0200

    Finished First Prototype

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ScreenComponent } from './components/screen/screen.component';
+import { ScreenComponent} from "./components/screen/screen.component";
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
     ScreenComponent
   ],
   imports: [
--- app/src/app/components/screen/screen.component.ts
+++ app/src/app/components/screen/screen.component.ts
@@ -1,33 +1,47 @@
-import {Component} from '@angular/core';
-import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+import {Component, OnInit} from '@angular/core';
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 
 @Component({
   selector: 'screen-view',
   templateUrl: './screen.component.html',
   styleUrls: ['./screen.component.css']
 })
-export class ScreenComponent {
-
-  iframeSrc: SafeResourceUrl;
-
+export class ScreenComponent implements OnInit {
+  public url: SafeResourceUrl = "http://localhost:5500";
+  public time: string = "";
 
   constructor(private sanitizer: DomSanitizer) {
-    this.iframeSrc = this.sanitizer.bypassSecurityTrustResourceUrl('');
   }
 
-  update(value: string) {
 
-    if (value == "4200") {
-      this.iframeSrc = this.sanitizer.bypassSecurityTrustResourceUrl("http://localhost:4200/");
-
-    }
-    if (value == "5500") {
-      this.iframeSrc = this.sanitizer.bypassSecurityTrustResourceUrl("http://localhost:5500/");
-
-    } else {
-      this.iframeSrc = this.sanitizer.bypassSecurityTrustResourceUrl(value);
-
-    }
+  loadURL(url: string) {
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+    localStorage.setItem("url", url)
 
   }
+
+
+  readonly deviceList: string[] = ["IPhone", "MacBook", "IMac"];
+
+  changeDevice(device: string) {
+    console.log(device);
+    localStorage.setItem("device-name", device);
+    location.reload();
+  }
+
+  ngOnInit() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    this.time = `${hours}:${minutes}`
+
+
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(localStorage.getItem("url")!);
+
+    // @ts-ignore
+
+    // @ts-ignore
+  }
+
+  protected readonly localStorage = localStorage;
 }
